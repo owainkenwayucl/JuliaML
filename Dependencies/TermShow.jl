154ba90dc0c6a356598b5122915751fa175f79ff
Owain Kenway <o.kenway@ucl.ac.uk>
2023-02-09 12:14:50 +0000

    Add half_height version

--- Dependencies/TermShow.jl
+++ Dependencies/TermShow.jl
@@ -4,7 +4,7 @@
 
 module TermShow
 
-	export render_greyscale_image
+	export render_greyscale_image, hires_render_greyscale_image
 
 	function render_greyscale_image(image)
 		num_colours = 25 
@@ -30,6 +30,45 @@ module TermShow
 				print(pixels[index])
 			end
 			println("")
+		end
+	end
+
+	function hires_render_greyscale_image(image)
+		num_colours = 25 
+		base_colour = 231
+		colours_int = fill(base_colour, num_colours)
+		pixels = fill(" ", num_colours)
+		for a in 1:num_colours-1 # every colour except the last one
+			colours_int[a] = base_colour + a
+		end
+
+		dimensions = size(image)
+
+		width = dimensions[1]
+		height = dimensions[2]
+
+
+		for y in 1:height ÷ 2
+			line = ""
+			slice = image[1:width,((2*y)-1):(2*y)]
+			for x in 1:width
+				index1 = Int(floor(slice[x,1] * (num_colours-1))) + 1
+				index2 = Int(floor(slice[x,2] * (num_colours-1))) + 1
+				pixel = "\033[48;5:"*string(colours_int[index1])*"m\033[38;5:"*string(colours_int[index2])*"m▄\033[m"
+				line = line*pixel
+			end
+			println(line)
+		end
+		if height % 2 == 1
+			line = ""
+			slice = image[1:width,height:height]
+			for x in 1:width
+				
+				index1 = Int(floor(slice[x,1] * (num_colours-1))) + 1
+				pixel = "\033[38;5:"*string(colours_int[index1])*"m▀\033[m"
+				line = line*pixel
+			end
+			println(line)
 		end
 	end
 
